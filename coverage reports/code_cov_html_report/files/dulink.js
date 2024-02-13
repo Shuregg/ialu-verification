142519010ef319c785407b6931755051911aab94
Alexander <san4ows55@gmail.com>
2024-02-13 20:46:09 +0300

    created coverage reports (code/func cover .txt files)

---
+++ coverage reports/code_cov_html_report/files/dulink.js
@@ -0,0 +1,2 @@
+var g_data = {"4":["work.scr1_pipe_ialu",24.56,1]};
+processDuLinks(g_data);
---
+++ coverage reports/code_cov_html_report/files/instlink.js
@@ -0,0 +1,2 @@
+var g_data = {"6":[5,"DUT",1],"5":[-1,"tb_scr1_pipe_ialu",1]};
+processInstLinks(g_data);
